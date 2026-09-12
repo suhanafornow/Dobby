8ee3a9e56925dc5dcc5f5a651faf7fca7a06657c
suhanafornow <150298936+suhanafornow@users.noreply.github.com>
2026-09-12 09:43:22 -0400

    paste inference.py

---
+++ inference.py
@@ -0,0 +1,23 @@
+# 1. Import the library
+from inference_sdk import InferenceHTTPClient, InferenceConfiguration
+
+# 2. Connect to your workflow
+client = InferenceHTTPClient(
+    api_url="https://serverless.roboflow.com",
+    api_key="9YQAmJF0LqKFP4fRvfoF"
+).configure(InferenceConfiguration(
+    api_key_transport="header"  # header-based auth (inference v1.5.0+)
+))
+
+# 3. Run your workflow on an image
+result = client.run_workflow(
+    workspace_name="suhanas-workspace-90vcb",
+    workflow_id="dobby-2-vdobby-2-raah5-2-rfdetr-2xlarge-t1-logic",
+    images={
+        "image": "YOUR_IMAGE.jpg" # Path to your image file
+    },
+    use_cache=True # Speeds up repeated requests
+)
+
+# 4. Get your results
+print(result)
